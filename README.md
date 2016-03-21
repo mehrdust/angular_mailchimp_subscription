6af8984f0511cb794bfac9d07744035bd4cbdf47
RSamimi <rayan@mehrdust.com>
2016-03-21 19:12:18 -0400

    updated to latest

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # MailChimp Subscription
 
+This demo app demonstrates the power of AngularJS for a simple newsletter subscription task. For the purpose of this demonstration MailChimp API is being used.
+
+For the API documentation for subscription click <a href="https://apidocs.mailchimp.com/api/2.0/lists/subscribe.php" role="button">here</a>
+
+This demo includes:
+<ul>
+	<li>AngularJS UI Router</li>
+	<li>Form validation with ngMessages</li>
+	<li>Angular Controller</li>
+	<li>Angular Factory Service</li>
+	<li>Angular $http service</li>
+	<li>Restful API call</li>
+	<li>Promise</li>
+</ul>
+
+<h2>Pre requisites</h2>
+<ul>
+	<li>nodeJS</li>
+	<li>npm</li>
+	<li>bower</li>
+	<li>karma cli</li>
+</ul>
+<h2>Setup Env</h2>
+
+to get this demo running follow these steps:
+
+<ol>
+	<li><pre>git clone https://github.com/mehrdust/angular_mailchimp_subscription.git</pre></li>
+	<li><pre>cd angular_mailchimp_subscription</pre></li>
+	<li><pre>npm install</pre></li>
+	<li><pre>bower install</pre></li>
+	<li><pre>gulp connect</pre></li>
+</ol>
--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,54 @@
 module.exports = function(config){
   config.set({
 
+    frameworks: ['jasmine'],
     basePath : './',
 
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/bower_components/angular-messages/angular-messages.min.js',
+      'app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+      'app/js/**/*.js'
     ],
 
     autoWatch : true,
 
-    frameworks: ['jasmine'],
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
-    browsers : ['Chrome'],
 
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+    // web server port
+    port: 9876,
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
 
   });
 };
